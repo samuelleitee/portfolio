8fc6fcb5345f6d5823f801fee9fe732ee2e2048c
samuelleitee <programmer.and.investor@gmail.com>
2024-02-29 21:38:52 -0300

    feat: created Aside component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
-    <>
+    <div className="App">
+      <Sidebar />
       <Header />
       <Footer />
-    </>
+    </div>
   );
 }
 
