1de2e4408b4e0f311ffc6069022486d1391a9efb
samuelleitee <programmer.and.investor@gmail.com>
2024-03-27 18:50:56 -0300

    feat: created Professional component

--- src/components/Informations/Professional/index.jsx
+++ src/components/Informations/Professional/index.jsx
@@ -1,5 +1,82 @@
+import styles from "./Professional.module.css"
+
 export function Professional() {
     return(
-        <p>PROFESSIONAL</p>
+        <section className={styles.section}>
+            <header className={styles.header}>personal-info</header>
+
+            <article className={styles.article}>
+                <aside>
+                    <span>1</span>
+                    <span>2</span>
+                    <span>3</span>
+                    <span>4</span>
+                    <span>5</span>
+                    <span>6</span>
+                    <span>7</span>
+                    <span>8</span>
+                    <span>9</span>
+                    <span>10</span>
+                    <span>11</span>
+                    <span>12</span>
+                    <span>13</span>
+                    <span>14</span>
+                    <span>15</span>
+                    <span>16</span>
+                </aside>
+
+                <div>
+                    <p>
+                        /**
+                    </p>
+                    <p>
+                        * About me
+                    </p>
+                    <p>
+                        * I have 5 years of еxperience in web
+                    </p>
+                    <p>
+                        * development lorem ipsum dolor sit amet,
+                    </p>
+                    <p>
+                        * consectetur adipiscing elit, sed do eiusmod
+                    </p>
+                    <p>
+                        * tempor incididunt ut labore et dolore
+                    </p>
+                    <p>
+                        * magna aliqua. Ut enim ad minim veniam,
+                    </p>
+                    <p>
+                        * quis nostrud exercitation ullamco laboris
+                    </p>
+                    <p>
+                        * nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <p>
+                        * Duis aute irure dolor in reprehenderit in
+                    </p>
+                    <p>
+                        *
+                    </p>
+                    <p>
+                        * Duis aute irure dolor in reprehenderit in
+                    </p>
+                    <p>
+                        * voluptate velit esse cillum dolore eu fugiat
+                    </p>
+                    <p>
+                        * nulla pariatur. Excepteur sint occaecat
+                    </p>
+                    <p>
+                        * officia deserunt mollit anim id est laborum.
+                    </p>
+                    <p>
+                        */
+                    </p>
+                </div>
+            </article>
+            <article className={styles.article}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo blanditiis alias provident sint, adipisci consequatur magnam laudantium, autem, eius quisquam fugiat cumque. Eum nihil, itaque veniam nam animi sed.</article>
+        </section>
     )
 }
